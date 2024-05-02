5cff865663679ab139eedfe752b524449a2cce41
maxi-gartner <gartner.maxi@hotmail.com>
2024-05-02 15:04:23 -0300

    :sparkles: agregando tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+/*         'primary':'#64748b',
+        'secondary':'#64748b',
+        'tertiary':'#94a3b8',
+        'quaternary':'#e2e8f0',
+        'quinary':'#f1f5f9',
+        'senary':'#f9fafb',
+        'septenary':'#f3f4f6',
+        'octonary':'#cbd5e1',
+        'nonary':'#94a3b8',
+        'denary':'#64748b', */
+      }
+    },
+  },
+  plugins: [],
+}
