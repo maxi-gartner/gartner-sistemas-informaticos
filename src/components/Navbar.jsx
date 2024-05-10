5568ea4d741cf2e3abbee82986f70601f0f80320
maxi-gartner <gartner.maxi@hotmail.com>
2024-05-10 02:30:53 -0300

    :sparkles: trabajando en sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,35 +14,49 @@ export default function Navbar() {
     return classes.filter(Boolean).join(' ')
   }
 
-    const [hidden, setHidden] = useState(false);
-  
-    const toggleVisibility = () => {
-      setHidden(!hidden);
-    };
+  const [open, setOpen] = useState(false)
+
+  const handleClick = () => {
+    setOpen(!open)
+  }
+
 
   return (
     <div>
-      {hidden ? (
-        <nav className="text-black h-16">
-            <div className="mx-auto w-full px-2 sm:px-6 lg:px-8">
-              <div className="flex h-16 items-center justify-between">
-                <div className="flex items-center sm:hidden min-w-60 bg-slate-500">
-                  <button className="relative inline-flex items-center justify-center rounded-md p-2 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" onClick={toggleVisibility}>
+          <nav className="text-black h-16">
+            <div className="absolute w-full py-2 flex justify-end px-4 tablet:justify-center tablet:mx-0">
+              <img className="h-12 w-auto rounded-full" src={logo} alt="G-S-I Company"/>
+            </div>
+
+            <div className="mx-auto w-full px-2 sm:px-6 lg:px-8 z-50" style={{zIndex: open ? '0' : '50'}}>
+              <div className="flex h-16 items-center justify-between relative">
+                <div className="flex items-center sm:hidden pr-4 pb-4">
+                  {/* Mobile menu button */}
+                  <button className="relative inline-flex items-center justify-center rounded-md p-2 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" onClick={handleClick}>
+                    <span className="sr-only">Open main menu</span>
                     <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
                   </button>
-                  <div>
-                    <p>links</p>
-                  </div>
                 </div>
                 <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                  <div className="flex flex-shrink-0 items-center">
-                    <img
-                      className="h-12 w-auto rounded-full"
-                      src={logo} alt="G-S-I Company"
-                    />
+                  <div className="hidden sm:ml-6 sm:block">
+                    <div className="flex items-center h-12">
+                      {navigation.map((item) => (
+                        <a
+                          key={item.name}
+                          href={item.href}
+                          className={classNames(
+                            item.current ? 'bg-gray-900 text-white' : 'text-black hover:bg-black hover:text-white',
+                            'rounded-md px-3 py-2 text-sm font-medium mx-2'
+                          )}
+                          aria-current={item.current ? 'page' : undefined}
+                        >
+                          {item.name}
+                        </a>
+                      ))}
+                    </div>
                   </div>
                 </div>
-                <div className="flex items-center pr-2 sm:static sm:ml-6 sm:pr-0 lg: min-w-60 bg-slate-500">
+                <div className="items-center pr-2 sm:static sm:ml-6 sm:pr-0 hidden tablet:flex">
                   <p className='mr-5 font-semibold'>Nombre de usuario</p>
                   <img
                     className="h-8 w-8 rounded-full"
@@ -52,65 +66,36 @@ export default function Navbar() {
                 </div>
               </div>
             </div>
+            
+            {/* Menu de navegación */}
+            <div className='absolute w-full  h-full bg-black bg-opacity-80 z-40 px-2' style={{display: open ? 'none' : 'block'}}>
+              <div className="bg-[#F2F2F2] text-black w-64 min-h-[50%] fixed top-0 left-0 overflow-y-auto pt-1 rounded-r-xl">
+                <button className="relative inline-flex items-center justify-center rounded-md p-2 ml-2  hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" onClick={handleClick}>
+                    <span className="sr-only">Open main menu</span>
+                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                  </button>
+                <div className="border-t-2 mt-5 border-black">
+                  <h1 className=" py-4 px-6 text-lg font-semibold">Mi Aplicación</h1>
+                </div>
+                
+                {/* Lista de enlaces */}
+                <nav>
+                  <ul className="space-y-2">
+                    <li>
+                      <a href="#" className="block py-2 px-4">Inicio</a>
+                    </li>
+                    <li>
+                      <a href="#" className="block py-2 px-4">Perfil</a>
+                    </li>
+                    <li>
+                      <a href="#" className="block py-2 px-4">Configuración</a>
+                    </li>
+                    {/* Agrega más enlaces según sea necesario */}
+                  </ul>
+                </nav>
+              </div>
+            </div>
           </nav>
-) 
-: (
-  <nav>
-        <div className="flex">
-  {/* Barra lateral */}
-  <div className="fixed left-0 top-0 h-full bg-gray-900 text-white w-64 flex flex-col justify-between z-50">
-    {/* Contenido de la barra lateral */}
-    <div className="p-4">
-      {/* Logo u otro contenido */}
-      <div className="mb-4">
-        <h1 className="text-xl font-bold">Mi Aplicación</h1>
-        {/* Otras partes del logo o texto */}
-      </div>
-      {/* Links de la barra lateral */}
-      <nav>
-        <ul>
-          <li className="py-2 hover:bg-gray-800">
-            <a href="#" className="block px-4">Inicio</a>
-          </li>
-          <li className="py-2 hover:bg-gray-800">
-            <a href="#" className="block px-4">Perfil</a>
-          </li>
-          <li className="py-2 hover:bg-gray-800">
-            <a href="#" className="block px-4">Configuración</a>
-          </li>
-          {/* Agregar más elementos según sea necesario */}
-        </ul>
-      </nav>
-    </div>
-    {/* Pie de página de la barra lateral */}
-    <div className="p-4">
-      <p className="text-sm">© 2024 Mi Empresa</p>
-    </div>
-  </div>
-  {/* Contenido de la página */}
-  <div className="ml-64"> {/* Aquí ajustamos el margen izquierdo para dejar espacio para la barra lateral */}
-    {/* Contenido de la página */}
-  </div>
-</div>
-  </nav>
-)}
     </div>
   )
 }
-
-
-{/* <div className="flex items-center h-12">
-{navigation.map((item) => (
-  <a
-    key={item.name}
-    href={item.href}
-    className={classNames(
-      item.current ? 'bg-gray-900 text-white' : 'text-black hover:bg-black hover:text-white',
-      'rounded-md px-3 py-2 text-sm font-medium mx-2'
-    )}
-    aria-current={item.current ? 'page' : undefined}
-  >
-    {item.name}
-  </a>
-))}
-</div> */}
