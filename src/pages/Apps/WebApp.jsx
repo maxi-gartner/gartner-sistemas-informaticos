6588028314814eddcc2b2f846f7271561f839c03
maxi-gartner <gartner.maxi@hotmail.com>
2024-07-02 23:49:59 -0300

    2/7

--- src/pages/Apps/WebApp.jsx
+++ src/pages/Apps/WebApp.jsx
@@ -25,7 +25,7 @@ export default function WebApp() {
                       <img src="public/bug-fix.png" className="object-cover rounded-xl" alt="" />
                       <div className='w-full h-full bg-black absolute top-0 left-0 flex justify-center items-center bg-opacity-40 hover:bg-opacity-0'>
                           <div className="flex flex-col justify-center items-center p-10 pb-16">
-                              <h2 className="text-white text-xl">
+                              <h2 className="text-white text-xl"> 
                                   {/* Texto */}  
                               </h2> 
                           </div> 
